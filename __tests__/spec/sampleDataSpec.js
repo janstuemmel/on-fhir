f297bfb7eea43cfe670760b67b6599c9edf7d07a
Jan <info@janstuemmel.de>
2017-05-26 20:00:56 +0200

    fix(sampleData): fixed test for sampleData

--- __tests__/spec/sampleDataSpec.js
+++ __tests__/spec/sampleDataSpec.js
@@ -5,14 +5,25 @@ describe('sampleData', () => {
   it('should validate WeightSample', () => {
 
     // when
-    var Observation = Weight.getFhirObject(100);
+    var Observation = Weight.getFhirObject({
+      value: 100,
+      unit: 'gram',
+      startDate: new Date().toISOString()
+    });
 
     // then
     expect(Observation).toMatchObject({
       resourceType: 'Observation',
       status: 'final',
       code: expect.objectContaining({
-        text: 'Weight'
+        text: 'Weight',
+        coding: expect.arrayContaining([
+          {
+            system: 'http://loinc.org',
+            code: '29463-7',
+            display: 'Body Weight',
+          }
+        ])
       }),
       valueQuantity: expect.objectContaining({
         value: 100,
